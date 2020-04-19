ce5c2a157bb8cea07cd46c89deb70863a62be1c7
Sahan Thinusha <sahanvijaya@gmail.com>
2020-04-19 20:23:31 +0530

    Added app and main tp

--- pkg/env/const.go
+++ pkg/env/const.go
@@ -6,4 +6,4 @@ const API_PATH string= "/src/api/"
 const API_SUFFIX string= "_api_service"+ DartExtension
 const MODEL_SUFFIX string= ""+ DartExtension
 const DartExtension = ".dart"
-const FormViewSuffix string = ""+DartExtension  
+const FormviewSuffix string = ""+DartExtension
